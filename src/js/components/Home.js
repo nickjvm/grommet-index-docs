708be26a18350231db8f1c4d5a663b0e0ce21e1c
Alan Souza <alansouzati@gmail.com>
2016-03-29 22:28:07 -0700

    Added docs for hello world codepen.

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,6 +1,9 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
 import React, { Component } from 'react';
+
+import Section from 'grommet/components/Section';
+
 import DocsArticle from './DocsArticle';
 
 export default class Home extends Component {
@@ -14,6 +17,27 @@ export default class Home extends Component {
           amounts of data that needs searching, filtering, sorting, etc. then
           Grommet Index can help.</p>
 
+        <Section primary={true}>
+          <h2>Hello World</h2>
+
+          <p>
+            To help you get started quickly we provide the following <a href="http://codepen.io/grommet/pen/PNKGxN/" target="_blank">
+              codepen
+            </a> template:
+          </p>
+          <iframe height='268' scrolling='no'
+            src='//codepen.io/grommet/embed/PNKGxN/?height=268&theme-id=0&default-tab=result'
+            frameBorder='no' allowTransparency='true' allowFullScreen='true'
+            style={{width: '100%'}}>See the Pen
+            <a href='http://codepen.io/grommet/pen/PNKGxN/'>Grommet Hello World</a>
+            by Grommet UX (<a href='http://codepen.io/grommet'>@grommet</a>) on
+            <a href='http://codepen.io'>CodePen</a>.
+          </iframe>
+          <p>
+            Also, there is a Github <a href="https://gist.github.com/grommetux/4b73162123b70103b763735c3ef58015"
+            target="_blank">Gist</a> that you can use to load this same Hello World app in your local computer.
+          </p>
+        </Section>
       </DocsArticle>
     );
   }
